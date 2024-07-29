a0ab8c5d80695ed1d825fd5ac6bc64928092e11c
dennismatz <dmatambanadzo@absp.online>
2024-07-29 16:35:07 +0200

    reversed a string

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
-        list.addLast(40);
-        list.addLast(50);
-        list.getKthNodeFromTheEnd(3);
+        StringReverser reverser = new StringReverser();
+
+        reverser.reverse(null);
 
     }
 }
---
+++ src/StringReverser.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class StringReverser {
+
+
+    public void reverse(String input) {
+        if (input == null)
+            throw new IllegalArgumentException();
+
+        StringBuilder reverseStr = new StringBuilder();
+
+        Stack<Character> niceStack = new Stack<>();
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            niceStack.push(input.charAt(i));
+            reverseStr.append(niceStack.pop());
+        }
+        System.out.println(reverseStr);
+    }
+}
