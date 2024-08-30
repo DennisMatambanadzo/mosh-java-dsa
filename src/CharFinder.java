db376e27f79280faed9b1dd189d695d221646c13
matambanadzotanakad <49873792+matazdt@users.noreply.github.com>
2024-08-31 00:51:52 +0200

    find first repeating character

---
+++ src/CharFinder.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class CharFinder {
+
+    public char findFirstNonRepeatingChar(String words) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        var chars = words.toCharArray();
+
+
+        for (var ch : chars) {
+            var count = map.containsKey(ch) ? map.get(ch) : 0;
+            map.put(ch, count + 1);
+        }
+        for (var ch : chars) {
+            if (map.get(ch) == 1)
+                return ch;
+        }
+        return Character.MIN_VALUE;
+    }
+
+    public char findFirstRepeatingCharacter(String words){
+
+        Set<Character> set = new HashSet<>();
+        var chars = words.toCharArray();
+        for (var ch: chars){
+            if(set.contains(ch))
+                return ch;
+            set.add(ch);
+        }
+        return Character.MIN_VALUE;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        PriorityQueue pq = new PriorityQueue();
+        CharFinder charFinder = new CharFinder();
+        var words = "a green apple";
+        var ch = charFinder.findFirstNonRepeatingChar(words);
+        System.out.println(ch);
 
-
-        pq.enqueue(5);
-        pq.enqueue(3);
-        pq.enqueue(6);
-        pq.enqueue(7);
-        pq.enqueue(4);
-        pq.enqueue(3);
-        pq.enqueue(2);
-
-        System.out.println(pq);
-
-        while (!pq.isEmpty())
-            System.out.println(pq.remove());
-
-
+        var ch2 = charFinder.findFirstRepeatingCharacter(words);
+        System.out.println(ch2);
     }
 }
