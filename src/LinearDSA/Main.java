f100327c2aa39968731f27f7a6bec707b17a1708
matambanadzotanakad <49873792+matazdt@users.noreply.github.com>
2024-09-16 11:00:25 +0200

    TreeRedo height

--- src/LinearDSA/Main.java
+++ src/LinearDSA/Main.java
@@ -8,16 +8,17 @@ public class Main {
     public static void main(String[] args) {
         TreeRedo tree = new TreeRedo();
 
-        tree.insert(7);
-        tree.insert(4);
-        tree.insert(9);
-        tree.insert(1);
-        tree.insert(6);
-        tree.insert(8);
-        tree.insert(10);
+//        tree.insert(7);
+//        tree.insert(4);
+//        tree.insert(9);
+//        tree.insert(1);
+//        tree.insert(6);
+//        tree.insert(8);
+//        tree.insert(10);
 
-        System.out.println(tree.find(3));
-
+//        System.out.println(tree.find(3));
+//        tree.traversePostOrder();
+        System.out.println(tree.min());
 
 
     }
--- src/NonLinearDSA/TreeRedo.java
+++ src/NonLinearDSA/TreeRedo.java
@@ -26,7 +26,6 @@ public class TreeRedo {
         var node = new Node(value);
         if (root == null) {
             root = node;
-            System.out.println(root.value);
             return;
         }
 
@@ -49,21 +48,21 @@ public class TreeRedo {
         }
     }
 
-    public boolean find(int value){
+    public boolean find(int value) {
 
         var current = root;
-        while (root!=null) {
+        while (root != null) {
             if (value < current.value) {
                 if (current.leftChild == null) {
                     return false;
-                }else if (current.leftChild.value==value){
+                } else if (current.leftChild.value == value) {
                     return true;
                 }
                 current = current.leftChild;
             } else {
                 if (current.rightChild == null) {
                     return false;
-                }else if (current.rightChild.value==value){
+                } else if (current.rightChild.value == value) {
                     return true;
                 }
                 current = current.rightChild;
@@ -71,5 +70,74 @@ public class TreeRedo {
 
         }
         return false;
+    }
+
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    private void traversePreOrder(Node root) {
+        if (root == null)
+            return;
+        System.out.println(root.value);
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+
+
+    }
+
+    public void traverseInOrder() {
+        traverseInOrder(root);
+    }
+
+    private void traverseInOrder(Node root) {
+        if (root == null)
+            return;
+        traverseInOrder(root.leftChild);
+        System.out.println(root.value);
+        traverseInOrder(root.rightChild);
+    }
+
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    private void traversePostOrder(Node root) {
+        if (root == null)
+            return;
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.println(root.value);
+    }
+
+    public int height() {
+        if (root == null)
+            return -1;
+        return height(root);
+    }
+
+    private int height(Node root) {
+        if (isLeaf(root))
+            return 0;
+        return 1 + Math.max(height(root.leftChild), height(root.rightChild));
+    }
+
+    public int min() {
+        if(root==null)
+            throw new IllegalStateException();
+        return min(root);
+    }
+
+    private int min(Node root) {
+        if (isLeaf(root))
+            return root.value;
+        var left = min(root.leftChild);
+        var right = min(root.rightChild);
+        return Math.min(Math.min(left, right), root.value);
+    }
+
+    private boolean isLeaf(Node node) {
+        return (node.leftChild == null && node.rightChild == null);
+
     }
 }
