23a225dd33901b3e5ce091afb3a8905278e9978d
matambanadzotanakad <49873792+matazdt@users.noreply.github.com>
2024-09-17 10:16:34 +0200

    completed binary tree section

--- src/LinearDSA/Main.java
+++ src/LinearDSA/Main.java
@@ -8,17 +8,24 @@ public class Main {
     public static void main(String[] args) {
         TreeRedo tree = new TreeRedo();
 
-//        tree.insert(7);
-//        tree.insert(4);
-//        tree.insert(9);
-//        tree.insert(1);
-//        tree.insert(6);
-//        tree.insert(8);
-//        tree.insert(10);
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+        TreeRedo tree2 = new TreeRedo();
 
-//        System.out.println(tree.find(3));
-//        tree.traversePostOrder();
-        System.out.println(tree.min());
+        tree2.insert(7);
+        tree2.insert(4);
+//        tree2.insert(9);
+        tree2.insert(1);
+        tree2.insert(6);
+        tree2.insert(8);
+        tree2.insert(10);
+
+        tree.traverseLevelOrder();
 
 
     }
--- src/NonLinearDSA/TreeRedo.java
+++ src/NonLinearDSA/TreeRedo.java
@@ -1,5 +1,7 @@
 package NonLinearDSA;
 
+import java.util.ArrayList;
+
 public class TreeRedo {
 
     private class Node {
@@ -13,9 +15,7 @@ public class TreeRedo {
 
         @Override
         public String toString() {
-            return "Node{" +
-                    "value=" + value +
-                    '}';
+            return "Node{" + "value=" + value + '}';
         }
     }
 
@@ -77,8 +77,7 @@ public class TreeRedo {
     }
 
     private void traversePreOrder(Node root) {
-        if (root == null)
-            return;
+        if (root == null) return;
         System.out.println(root.value);
         traversePreOrder(root.leftChild);
         traversePreOrder(root.rightChild);
@@ -91,46 +90,40 @@ public class TreeRedo {
     }
 
     private void traverseInOrder(Node root) {
-        if (root == null)
-            return;
+        if (root == null) return;
         traverseInOrder(root.leftChild);
         System.out.println(root.value);
         traverseInOrder(root.rightChild);
-    }
+}
 
     public void traversePostOrder() {
         traversePostOrder(root);
     }
 
     private void traversePostOrder(Node root) {
-        if (root == null)
-            return;
+        if (root == null) return;
         traversePostOrder(root.leftChild);
         traversePostOrder(root.rightChild);
         System.out.println(root.value);
     }
 
     public int height() {
-        if (root == null)
-            return -1;
+        if (root == null) return -1;
         return height(root);
     }
 
     private int height(Node root) {
-        if (isLeaf(root))
-            return 0;
+        if (isLeaf(root)) return 0;
         return 1 + Math.max(height(root.leftChild), height(root.rightChild));
     }
 
     public int min() {
-        if(root==null)
-            throw new IllegalStateException();
+        if (root == null) throw new IllegalStateException();
         return min(root);
     }
 
     private int min(Node root) {
-        if (isLeaf(root))
-            return root.value;
+        if (isLeaf(root)) return root.value;
         var left = min(root.leftChild);
         var right = min(root.rightChild);
         return Math.min(Math.min(left, right), root.value);
@@ -138,6 +131,66 @@ public class TreeRedo {
 
     private boolean isLeaf(Node node) {
         return (node.leftChild == null && node.rightChild == null);
+
+    }
+
+    public boolean equals(TreeRedo other) {
+        if (other == null)
+            return false;
+        return equals(root, other.root);
+    }
+
+    private boolean equals(Node first, Node second) {
+        if (first == null && second == null) return true;
+
+        if (first != null && second != null)
+            return first.value == second.value
+                    && equals(first.leftChild, second.leftChild)
+                    && equals(first.rightChild, second.rightChild);
+        return false;
+    }
+
+    public boolean isBinarySearchTree() {
+        return isBinarySearchTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean isBinarySearchTree(Node root, int min, int max) {
+        if (root == null)
+            return true;
+
+        if (root.value < min || root.value > max) {
+            return false;
+        }
+        return isBinarySearchTree(root.leftChild, min, root.value - 1) &&
+                isBinarySearchTree(root.rightChild, root.value + 1, max);
+    }
+
+    public ArrayList<Integer> getNodesAtDistance(int distance) {
+        var list = new ArrayList<Integer>();
+        getNodesAtDistance(root, distance, list);
+        return list;
+
+    }
+
+    private void getNodesAtDistance(Node root, int distance, ArrayList<Integer> list) {
+
+        if (root == null)
+            return;
+        if (distance == 0) {
+            list.add(root.value);
+            return;
+        }
+
+        getNodesAtDistance(root.leftChild, distance - 1, list);
+        getNodesAtDistance(root.rightChild, distance - 1, list);
+    }
+
+    public void traverseLevelOrder() {
+        for (int i = 0; i < height(); i++) {
+            for (var value : getNodesAtDistance(i))
+                System.out.println(value);
+
+        }
 
     }
 }
